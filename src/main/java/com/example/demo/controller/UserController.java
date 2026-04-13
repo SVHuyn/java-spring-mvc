10981dbafaa154bace7dde58b9ea9f0fb28cb811
huynh is alone <huynhronaldo123@gmail.com>
2026-04-14 00:28:15 +0700

    update

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,30 +1,36 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.service.UserService;
 
 
-// @Controller
-// public class UserController {
-    
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello hehe";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
+
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    @GetMapping("/")
+    
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        return "eric.html";
     }
 }
+
+// @RestController
+// public class UserController {
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @GetMapping("/")
+//     public String getHomePage() {
+//         return this.userService.handleHello();
+//     }
+// }
